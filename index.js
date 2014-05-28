923f0129aa438990fa12e0abd0966768b8bc2ef4
MikeB <michael@vast.com>
2014-05-27 21:03:42 -0500

    updated exports to export utilities

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 var createFactory = require('./lib/factory');
+var configmasticator = require('./lib/configmasticator');
+var reqMod = require('./lib/pluginRetrievers.js').module;
 
+//-------------------------------------------------------------------------------------------------
+// factory exports
+//-------------------------------------------------------------------------------------------------
 module.exports.createBuilder = createFactory;
 
-var configmasticator = require('./lib/configmasticator');
-
+//-------------------------------------------------------------------------------------------------
+// config exports
+//-------------------------------------------------------------------------------------------------
 module.exports.config = {
 	'overlay' : configmasticator.overlay,
 	'trimout' : configmasticator.trimout
+};
+
+//-------------------------------------------------------------------------------------------------
+// utility exports (helpers for making retrievers)
+//-------------------------------------------------------------------------------------------------
+module.exports.utilities = {
+	'requireModule' : reqMod
 };
