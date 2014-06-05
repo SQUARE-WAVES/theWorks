15089180527e027adc1250c950c227e6d75e6365
MikeB <michael@vast.com>
2014-06-05 00:58:37 -0500

    removed config masticator, it should be it's own module anyways

--- index.js
+++ index.js
@@ -1,19 +1,10 @@
 var createFactory = require('./lib/factory');
-var configmasticator = require('./lib/configmasticator');
 var reqMod = require('./lib/pluginRetrievers.js').module;
 
 //-------------------------------------------------------------------------------------------------
 // factory exports
 //-------------------------------------------------------------------------------------------------
 module.exports.createBuilder = createFactory;
-
-//-------------------------------------------------------------------------------------------------
-// config exports
-//-------------------------------------------------------------------------------------------------
-module.exports.config = {
-	'overlay' : configmasticator.overlay,
-	'trimout' : configmasticator.trimout
-};
 
 //-------------------------------------------------------------------------------------------------
 // utility exports (helpers for making retrievers)
