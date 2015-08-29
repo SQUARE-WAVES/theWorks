99ded59e62277309690069b2d1aa17fb161bfd6f
SQUARE-WAVES <michael.behrman@gmail.com>
2015-08-28 19:46:38 -0500

    added new retrievers changed index to export new guys

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 var createBuilder = require('./lib/builder.js');
+var retrievers = require("./lib/pluginRetrievers.js")
+var reqMod =require("./lib/reqMod.js");
 
 //-------------------------------------------------------------------------------------------------
-// factory exports
+// factory export
 //-------------------------------------------------------------------------------------------------
 module.exports.createBuilder = createBuilder;
+
+//-------------------------------------------------------------------------------------------------
+//  default retrievers
+//  these are exported as a new object
+// 	because of how require works.
+//  I don't want users being able to change the meaning of "plugin" or "value"
+//  for all builders ever, just for the one they are building right now
+//-------------------------------------------------------------------------------------------------
+module.exports.retrierves= {
+	"plugin": retrievers.plugin,
+	"value":retrievers.value,
+	"req":retrievers.req,
+	"package":retrievers.package
+};
+
+module.exports.utilities= {
+	"reqMod":reqMod
+}
