db5ac826ee52beac9093801077133a4926e549de
mike-b <mbehrman@cbancnetwork.com>
2016-05-19 21:57:15 -0500

    added error logging helper

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var createBuilder = require('./lib/builder.js');
 var retrievers = require("./lib/pluginRetrievers.js")
-var reqMod =require("./lib/reqMod.js");
+var reqMod = require("./lib/reqMod.js");
+var logError = require("./lib/logErrorStack.js");
 
 //-------------------------------------------------------------------------------------------------
 // factory export
@@ -14,13 +15,14 @@ module.exports.createBuilder = createBuilder;
 //  I don't want users being able to change the meaning of "plugin" or "value"
 //  for all builders ever, just for the one they are building right now
 //-------------------------------------------------------------------------------------------------
-module.exports.retrierves= {
-	"plugin": retrievers.plugin,
-	"value":retrievers.value,
-	"req":retrievers.req,
-	"package":retrievers.package
+module.exports.retrierves = {
+  "plugin": retrievers.plugin,
+  "value": retrievers.value,
+  "req": retrievers.req,
+  "package": retrievers.package
 };
 
-module.exports.utilities= {
-	"reqMod":reqMod
+module.exports.utilities = {
+  "reqMod": reqMod,
+  "logError":
 }
